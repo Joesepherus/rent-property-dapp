a013b697fb6bea016a6f5e10ff4b2e7e02b8b553
Joseph Maloch <jozefmaloch@gmail.com>
2024-04-30 13:45:25 +0800

    Add getSinger, getContractCount to communicate with the contract and call functions upon it.

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,51 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import RentProperty from "./artifacts/contracts/RentProperty.sol/RentProperty.json";
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+const CONTRACT_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 
 function App() {
+  const [account, setAccount] = useState();
+  const [signer, setSigner] = useState();
 
 
-  return (
-    <div className="appContainer">
+  useEffect(() => {
+    async function getSinger() {
+      const accounts = await provider.send("eth_requestAccounts", []);
 
-    </div>
+      setAccount(accounts[0]);
+      setSigner(provider.getSigner());
+    }
+
+    getSinger();
+  }, [account]);
+
+  const contract = new ethers.Contract(
+    CONTRACT_ADDRESS,
+    RentProperty.abi,
+    provider
   );
+  const contractWithSigner = contract.connect(signer);
+
+
+
+  async function getContractCount() {
+    const _contractCount = parseInt(
+      (await contractWithSigner.contractCount())._hex
+    );
+    console.log('_contractCount: ', _contractCount);
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      getContractCount()
+    }
+    fetchData();
+  }, [signer]);
+
+  return <div className="appContainer"></div>;
 }
 
 export default App;
