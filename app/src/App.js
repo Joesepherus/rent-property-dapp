e473aa6586e0e978301b5aa85d2e6195c9e796a5
Joseph Maloch <jozefmaloch@gmail.com>
2024-04-30 14:32:55 +0800

    Add getPropertiesCount.

--- app/src/App.js
+++ app/src/App.js
@@ -9,7 +9,8 @@ const CONTRACT_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 function App() {
   const [account, setAccount] = useState();
   const [signer, setSigner] = useState();
-
+  const [contractCount, setContractCount] = useState();
+  const [propertiesCount, setPropertiesCount] = useState();
 
   useEffect(() => {
     async function getSinger() {
@@ -29,23 +30,51 @@ function App() {
   );
   const contractWithSigner = contract.connect(signer);
 
-
-
   async function getContractCount() {
     const _contractCount = parseInt(
       (await contractWithSigner.contractCount())._hex
     );
-    console.log('_contractCount: ', _contractCount);
+    setContractCount(_contractCount)
+    console.log("_contractCount: ", _contractCount);
+  }
+
+  async function getPropertiesCount() {
+    const _propertiesCount = parseInt(
+      (await contractWithSigner.propertiesCount())._hex
+    );
+    setPropertiesCount(_propertiesCount)
+
+    console.log("_propertiesCount: ", _propertiesCount);
   }
 
   useEffect(() => {
     async function fetchData() {
-      getContractCount()
+      getContractCount();
+      getPropertiesCount();
     }
     fetchData();
   }, [signer]);
 
-  return <div className="appContainer"></div>;
+  return (
+    <div className="appContainer">
+      <h1>Rent Property dApp</h1>
+      <h2>What's this dApp about?</h2>
+      <div className="description">
+        add later
+      </div>
+      <div className="contractContainer">
+        <div className="contract">
+          <h2>Contracts</h2>
+          <div>Contracts count: {contractCount}</div>
+        </div>
+        <div className="contract">
+          <h2>Properties</h2>
+          <div>Properties count: {propertiesCount}</div>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
 
 export default App;
